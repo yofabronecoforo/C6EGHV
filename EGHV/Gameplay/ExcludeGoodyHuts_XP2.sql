cd9e2182ecc56e1c72eada0426f63557a20ee0d8
zzragnar0kzz <60903639+zzragnar0kzz@users.noreply.github.com>
2021-05-22 21:05:53 -0700

    Add files via upload
    revamped content detection system (again)

--- EGHV/Gameplay/ExcludeGoodyHuts_XP2.sql
+++ EGHV/Gameplay/ExcludeGoodyHuts_XP2.sql
@@ -14,7 +14,7 @@
 -- refresh the overall weight of Expansion 2 type(s) to account for any exclusions
 UPDATE GoodyHuts SET Weight = (SELECT SUM(Weight) FROM GoodyHutSubTypes WHERE GoodyHut = 'GOODYHUT_DIPLOMACY') WHERE GoodyHutType = 'GOODYHUT_DIPLOMACY';
 
--- refresh the overall weight of the meteor strike type based on the sum of military subtype weights
+-- refresh the overall weight of the meteor strike type
 UPDATE GoodyHuts SET Weight = (SELECT SUM(Weight) FROM GoodyHutSubTypes WHERE GoodyHut = 'METEOR_GOODIES') WHERE GoodyHutType = 'METEOR_GOODIES';
 -- UPDATE GoodyHutSubTypes SET Weight = (SELECT Weight FROM GoodyHuts WHERE GoodyHutType = 'METEOR_GOODIES') WHERE SubTypeGoodyHut = 'METEOR_GRANT_GOODIES';
 
