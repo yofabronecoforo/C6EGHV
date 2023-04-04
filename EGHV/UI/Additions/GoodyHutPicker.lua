b593203a08ae10a1387dc0132f27cc308153475f
zzragnar0kzz <60903639+zzragnar0kzz@users.noreply.github.com>
2023-04-04 00:58:13 -0700

    Update GoodyHutPicker

--- EGHV/UI/Additions/GoodyHutPicker.lua
+++ EGHV/UI/Additions/GoodyHutPicker.lua
@@ -1,6 +1,6 @@
 --[[ =========================================================================
 	EGHV : Enhanced Goodies and Hostile Villagers for Civilization VI
-	Copyright (C) 2020-2021 zzragnar0kzz
+	Copyright (C) 2020-2023 zzragnar0kzz
 	All rights reserved
 =========================================================================== ]]
 
