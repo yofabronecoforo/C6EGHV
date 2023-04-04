7dd82f464eb1a9e840205e48f081b9c9897156fb
zzragnar0kzz <60903639+zzragnar0kzz@users.noreply.github.com>
2023-04-04 00:40:31 -0700

    Update ExcludeGoodyHut/Survivors

--- EGHV/Gameplay/ExcludeGoodyHut/Survivors/Add_Population.sql
+++ EGHV/Gameplay/ExcludeGoodyHut/Survivors/Add_Population.sql
@@ -1,6 +1,6 @@
 /* ###########################################################################
     EGHV : Enhanced Goodies and Hostile Villagers for Civilization VI
-    Copyright (c) 2020-2021 zzragnar0kzz
+    Copyright (c) 2020-2023 zzragnar0kzz
     All rights reserved.
 ########################################################################### */
 
@@ -8,7 +8,7 @@
     begin EGHV exclude Goody Hut configuration
 ########################################################################### */
 
--- exclude the add population to nearest city subtype
+-- exclude the add new population subtype
 UPDATE GoodyHutSubTypes SET Weight = 0 WHERE SubTypeGoodyHut = 'GOODYHUT_ADD_POP';
 
 /* ###########################################################################
