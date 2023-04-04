65b40391b42f6903057243756abf2a2df4693f57
zzragnar0kzz <60903639+zzragnar0kzz@users.noreply.github.com>
2023-04-04 00:38:55 -0700

    Update ExcludeGoodyHut/Secrets

--- EGHV/Gameplay/ExcludeGoodyHut/Secrets/Villager_Secrets_Production.sql
+++ EGHV/Gameplay/ExcludeGoodyHut/Secrets/Villager_Secrets_Production.sql
@@ -1,6 +1,6 @@
 /* ###########################################################################
     EGHV : Enhanced Goodies and Hostile Villagers for Civilization VI
-    Copyright (c) 2020-2021 zzragnar0kzz
+    Copyright (c) 2020-2023 zzragnar0kzz
     All rights reserved.
 ########################################################################### */
 
@@ -8,9 +8,8 @@
     begin EGHV exclude Goody Hut configuration
 ########################################################################### */
 
--- exclude the unlock villager secrets subtype
+-- exclude the production totem subtype
 UPDATE GoodyHutSubTypes SET Weight = 0 WHERE SubTypeGoodyHut = 'GOODYHUT_VILLAGER_SECRETS_PRODUCTION';
-UPDATE GoodyHuts Set Weight = (SELECT CASE WHEN (SELECT SUM(Weight) FROM GoodyHutSubTypes WHERE GoodyHut = 'GOODYHUT_SECRETS') = 0 THEN 0 ELSE (SELECT Weight FROM GoodyHuts WHERE GoodyHutType = 'GOODYHUT_SECRETS') END) WHERE GoodyHutType = 'GOODYHUT_SECRETS';
 
 /* ###########################################################################
     end EGHV exclude Goody Hut configuration
