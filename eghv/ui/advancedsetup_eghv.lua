37700af7017776f6ec93df6c05d67868c4e95557
zzragnar0kzz <60903639+zzragnar0kzz@users.noreply.github.com>
2024-05-11 18:46:39 -0700

    Move shared components to gamesetuplogic_eghv

--- eghv/ui/advancedsetup_eghv.lua
+++ eghv/ui/advancedsetup_eghv.lua
@@ -19,26 +19,5 @@ function HostGame()
 end
 
 --[[ =========================================================================
-	OVERRIDE: call pre-EGHV OnShutdown() and remove LuaEvent listeners for the Goody Hut picker
-=========================================================================== ]]
-Pre_EGHV_OnShutdown = OnShutdown;
-function OnShutdown()
-    Pre_EGHV_OnShutdown();
-	LuaEvents.GoodyHutPicker_SetParameterValues.Remove(OnSetParameterValues);
-	-- LuaEvents.GoodyHutPicker_SetParameterValue.Remove(OnSetParameterValue);
-end
-
---[[ =========================================================================
-	reset context pointers with modified functions
-=========================================================================== ]]
-ContextPtr:SetShutdown( OnShutdown );
-
---[[ =========================================================================
-	add new LuaEvent listeners for the Goody Hut picker
-=========================================================================== ]]
-LuaEvents.GoodyHutPicker_SetParameterValues.Add(OnSetParameterValues);
--- LuaEvents.GoodyHutPicker_SetParameterValue.Add(OnSetParameterValue);
-
---[[ =========================================================================
 	end advancedsetup_eghv.lua configuration script
 =========================================================================== ]]
